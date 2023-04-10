d78b01efe67b6b0618ab2a151a5b8a67850790a0
Sara <sara_breugelmans@hotmail.com>
2023-04-10 18:25:00 +0200

    initial migrations added

---
+++ database/migrations/2023_04_10_160533_create_lots_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lots', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTime('started_at');
+            $table->dateTime('ended_at');
+            $table->dateTime('published_at');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lots');
+    }
+};
---
+++ database/migrations/2023_04_10_160600_create_bids_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount', 8, 2);
+            $table->timestamp('bid_at');
+            $table->foreignId('item_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bids');
+    }
+};
